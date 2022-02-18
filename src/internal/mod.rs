cd2d7d68737aeea127ce04bc7489a41fe047f5d9
Mykola <ks2225@protonmail.com>
2022-02-18 20:43:16 +0200

    [Dev/Minor] Ran through cargo fmt --all

--- src/internal/mod.rs
+++ src/internal/mod.rs
@@ -1,3 +1,3 @@
+pub mod lingva_translate;
 pub mod ping;
 pub mod say;
-pub mod lingva_translate;
--- src/internal/say.rs
+++ src/internal/say.rs
@@ -1,10 +1,10 @@
 use crate::api::sanitization;
 
-pub async fn say(length: usize, clean_args_ref: &Vec<&str>) -> String{
-	if length == 1 {
-		"🌲 I do not have anything to say".to_owned()
-	} else {
-		let say = sanitization::sanitize_text(clean_args_ref[1..].join(" ").to_owned());
-		say.await.unwrap()
-	}
-} 
+pub async fn say(length: usize, clean_args_ref: &Vec<&str>) -> String {
+    if length == 1 {
+        "🌲 I do not have anything to say".to_owned()
+    } else {
+        let say = sanitization::sanitize_text(clean_args_ref[1..].join(" ").to_owned());
+        say.await.unwrap()
+    }
+}
