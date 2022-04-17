2c6934eba5b7396db4e9d3d51db77522eb4db7d4
Mykola <ks2225@protonmail.com>
2022-04-17 22:04:25 +0300

    fix: ran through cargo fmt

--- src/internal/say.rs
+++ src/internal/say.rs
@@ -4,7 +4,10 @@ pub async fn say(length: usize, clean_args_ref: &[&str], message_sender: &String
     if length == 1 {
         "🌲 I do not have anything to say".to_owned()
     } else {
-        let say = sanitization::sanitize_text_v2(clean_args_ref[1..].join(" "), Some(message_sender.to_string()));
+        let say = sanitization::sanitize_text_v2(
+            clean_args_ref[1..].join(" "),
+            Some(message_sender.to_string()),
+        );
         say.await.unwrap()
     }
 }
